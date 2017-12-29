d999aef2625194ce39ad149bfffa21e619019454
By <wzswzd@users.noreply.github.com>
2017-12-29 14:19:16 +0800

    上传了自有的导航页面

---
+++ others/index/myjavascript.js
@@ -0,0 +1,28 @@
+$(function() {
+    $('.nav a').each(
+
+    	function() {
+        var href = $(this).attr('href');
+        var count = window.localStorage.getItem(href);
+        count = count || 0;
+        $(this).append('<i>' + count + '</i>');
+        $(this).attr('target', '_blank');
+        $(this).click(
+
+        	function() {
+            var count = parseInt($(this).find('i').html()) + 1;
+            $(this).find('i').html(count);
+            window.localStorage.setItem(href, count);
+            console.log(count);
+        });
+        $(this).hover(
+
+        	function() {
+            $(this).parents('.nav').find('h3 i').html($(this).attr('href'));
+        },
+        
+        function() {
+            $(this).parents('.nav').find('h3 i').html('');
+        });
+    });
+});
